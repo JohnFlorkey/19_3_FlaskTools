6594d1d69c029770fe979670b060bdb23d0c4bd6
John Florkey <john.florkey@gmail.com>
2021-03-13 21:38:11 -0500

    implemented everything but session

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'notmuchofasecret'
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+survey = satisfaction_survey
+
+@app.route('/')
+def start():
+    responses.clear()
+    return render_template('start.html', title=survey.title, instructions=survey.instructions)
+
+@app.route('/questions/<int:index>')
+def question(index):
+    questions_answered = len(responses)
+    if not index == questions_answered:
+        flash('Naughty, naughty! No attempting access questions out of order!')
+        return redirect('/questions/' + str(questions_answered))
+    question = survey.questions[index]
+    return render_template('question.html', title=survey.title, question=question.question, choices=question.choices, index=index)
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    answer = request.form.get('answer')
+    index = int(request.form.get('index'))
+    responses.append(answer)
+    next_index = index + 1
+    if next_index >= len(survey.questions):
+        return redirect('/end')
+    else:
+        return redirect('/questions/' + str(next_index))
+
+@app.route('/end')
+def end():
+    return render_template('end.html')
