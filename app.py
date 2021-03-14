007515cf5939931a9b52487ab086650011777621
John Florkey <john.florkey@gmail.com>
2021-03-14 11:12:42 -0400

    added session to store responses on the client

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,17 +7,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'notmuchofasecret'
 debug = DebugToolbarExtension(app)
 
-responses = []
+# responses = []
 
 survey = satisfaction_survey
 
 @app.route('/')
 def start():
-    responses.clear()
+    # responses.clear()
     return render_template('start.html', title=survey.title, instructions=survey.instructions)
+
+@app.route('/session_init')
+def session_init():
+    responses = []
+    session['responses'] = responses
+    return redirect('/questions/0')
 
 @app.route('/questions/<int:index>')
 def question(index):
+    responses = session['responses']
     questions_answered = len(responses)
     if not index == questions_answered:
         flash('Naughty, naughty! No attempting access questions out of order!')
@@ -29,7 +36,9 @@ def question(index):
 def answer():
     answer = request.form.get('answer')
     index = int(request.form.get('index'))
+    responses = session['responses']
     responses.append(answer)
+    session['responses'] = responses
     next_index = index + 1
     if next_index >= len(survey.questions):
         return redirect('/end')
